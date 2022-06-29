7000a09a40f5329aefc6f3e0880753bf418d1496
Nickolas Thompson <thompsonnickolas1@gmail.com>
2022-06-29 18:52:24 -0400

    accidentally closed vscode and almost lost work, in the middle of creating category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,22 +1,48 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Category, Product, Tag, ProductTag} = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// get ALL categories
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [Product]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+// create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// get ONE category
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id,{
+      include: [Product]
+    })
+    if (!categoryData) {
+      res.status(404).json({ message: "No categories with this id found!" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+
+// update a category
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
